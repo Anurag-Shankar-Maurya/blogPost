b59392b36e58756261ca334d9a25a71773ff6c4b
anurag <anuragshankarmaurya@gmail.com>
2025-07-19 13:47:53 +0530

    fix(settings): update ALLOWED_HOSTS for local development
    Add common localhost addresses to ALLOWED_HOSTS to prevent Django from rejecting requests during development

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d^*&r3m@kvkc^6s^&-k83qowd+)j_(%yu(gfhfv8&almrk32w5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
